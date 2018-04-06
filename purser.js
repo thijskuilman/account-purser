e3d09889a6ae9088587debc52a962121acee412f
thijs <thijs.kuilman@gmail.com>
2018-04-06 14:33:34 +0200

    Added support for multiple search queries. Added counter.

--- purser.js
+++ purser.js
@@ -1,13 +1,14 @@
 var ownedAccounts = [];
 var list = document.getElementById('messageTable');
 var tableCount = 0;
+var accountCount = document.getElementById("accountCount");
 
 // Update UI after succesful sign in
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
-    searchMessages('Thank you for registering', getMessages);
+    searchMessages(searchQueries, getMessages);
   } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
@@ -18,33 +19,42 @@ function updateSigninStatus(isSignedIn) {
 function addAccountEntry(message) {
   tableCount++;
   var messageEntry = document.createElement('tr');
-  messageEntry.innerHTML = '<th scope="row">' + tableCount + '</th> <td>' + message.from + '</td> <td>' + message.to + '</td> <td><a target="_blank" href="http://' + message.website + '">' + message.website + '</a></td><td class="text-success table-success">Yes</td>';
+  messageEntry.innerHTML =  '<th scope="row">' + tableCount + '</th>' + 
+                            '<td>' + message.from + '</td>' + 
+                            '<td>' + message.to + '</td>' + 
+                            '<td><a target="_blank" href="http://' + message.website + '">' + message.website + '</a></td>'
+                            // '<td class="text-success table-success">Yes</td>'
+                            ;
   list.appendChild(messageEntry);
+  accountCount.innerText = tableCount;
 }
 
 // Search for messages in your inbox
-function searchMessages(query, callback) {
-  var getPageOfMessages = function(request, result) {
-    request.execute(function(resp) {
-      result = result.concat(resp.messages);
-      var nextPageToken = resp.nextPageToken;
-      if (nextPageToken) {
-        request = gapi.client.gmail.users.messages.list({
-          'userId': 'me',
-          'pageToken': nextPageToken,
-          'q': query
-        });
-        getPageOfMessages(request, result);
-      } else {
-        callback(result);
-      }
+function searchMessages(queries, callback) {
+  queries.forEach(function(query) {
+    var getPageOfMessages = function(request, result) {
+      request.execute(function(resp) {
+        result = result.concat(resp.messages);
+        var nextPageToken = resp.nextPageToken;
+        if (nextPageToken) {
+          request = gapi.client.gmail.users.messages.list({
+            'userId': 'me',
+            'pageToken': nextPageToken,
+            'q': query
+          });
+          getPageOfMessages(request, result);
+        } else {
+          console.log(result.length + " results for " + query)
+          callback(result);
+        }
+      });
+    };
+    var initialRequest = gapi.client.gmail.users.messages.list({
+      'userId': 'me',
+      'q': query
     });
-  };
-  var initialRequest = gapi.client.gmail.users.messages.list({
-    'userId': 'me',
-    'q': query
+    getPageOfMessages(initialRequest, []);
   });
-  getPageOfMessages(initialRequest, []);
 }
 
 // Get message details
---
+++ search_queries.js
@@ -0,0 +1,19 @@
+// These queries will be used to search your inbox for new accounts.
+// Make sure to add quotes to increase the search precision
+
+var searchQueries = [
+	'"confirm your email"',
+	'"bevestiging e-mailadres"',
+
+	'"Thank you for registering"',
+	'"Bedankt voor uw aanmelding"',
+
+	'"Your new account"', 
+	'"nieuw account"',
+
+	'"Welcome to"', 
+	'"Welkom bij"',
+
+	'activeer uw account',
+	'"confirm your account"',
+]
